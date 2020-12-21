e0f97493e43f50d4e443e4578c7a60b1a91c7a71
Mahesh Guntumadugu <g.maheshlearn@outlook.com>
2020-12-21 12:16:36 +0530

    comments added

--- DataStructuresAndAlgorithms/src/com/BigO/LogNtime.java
+++ DataStructuresAndAlgorithms/src/com/BigO/LogNtime.java
@@ -13,6 +13,7 @@ public class LogNtime {
 		
 		// 0(log n)
 		//binary search reduces input size by half and itis very quick
+		//algirithm with logarthimic time is more scalable than linear time
 		int result = Arrays.binarySearch(intArray, 2);
 		
 		System.out.println(result);

1885a79c9bfc70153a1cb7bc8adda52fdecd03fb
Mahesh Guntumadugu <g.maheshlearn@outlook.com>
2020-12-21 12:20:36 +0530

    exponential growth 2^n complexity

---
+++ DataStructuresAndAlgorithms/src/com/BigO/ExponentialGrowthTime.java
@@ -0,0 +1,13 @@
+package com.BigO;
+
+public class ExponentialGrowthTime {
+	
+	public static void main(String[] args) {
+		
+		
+		//O(2^n) is exactly opposite of O(log n)
+		
+		//when input size grows it is(O(2^n)) not scalable
+	}
+
+}

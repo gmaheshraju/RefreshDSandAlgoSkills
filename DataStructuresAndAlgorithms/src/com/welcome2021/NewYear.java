eb9d10605423a2ae97ca94205cff10361214ad9d
Mahesh Guntumadugu <g.maheshlearn@outlook.com>
2021-01-01 19:44:18 +0530

    test file

---
+++ DataStructuresAndAlgorithms/src/com/welcome2021/NewYear.java
@@ -0,0 +1,9 @@
+package com.welcome2021;
+
+public class NewYear {
+	
+	public static void main(String[] args) {
+		System.out.println("Start codin ! welcome to 2021.I will improve my coding skills");
+	}
+
+}
